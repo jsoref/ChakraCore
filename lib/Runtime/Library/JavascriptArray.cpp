2eaaa0a599db5d2d357e5e65781ed892ca18c164
Josh Soref <jsoref@users.noreply.github.com>
2016-01-27 22:39:58 +0000

    spelling: separately

--- lib/Runtime/Library/JavascriptArray.cpp
+++ lib/Runtime/Library/JavascriptArray.cpp
@@ -2966,7 +2966,7 @@ namespace Js
                     {
                         // This is a special case for spreadable objects. We do not pre-calculate the length
                         // in EntryConcat like we do with Arrays because a getProperty on an object Length
-                        // is observable. The result is we have to check for overflows seperately for
+                        // is observable. The result is we have to check for overflows separately for
                         // spreadable objects and promote to a bigger index type when we find them.
                         ConcatArgs<BigIndex>(pDestArray, remoteTypeIds, args, scriptContext, idxArg, idxDest, /*firstPromotedItemIsSpreadable*/true, length);
                         return;
